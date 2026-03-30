4d9dc9367c0db9bf0a465e11a798e55316285c52
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-03-29 22:26:48 -0500

    corrected google sheets key

--- src/action.ts
+++ src/action.ts
@@ -7,7 +7,7 @@ export async function getGoogleSheet() {
   try {
     const auth = new google.auth.JWT({
       email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+      key: process.env.GOOGLE_SHEETS_PRIVATE_KEY!.replace("\\n", "\n"),
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
