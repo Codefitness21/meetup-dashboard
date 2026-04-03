390daa86b1542acee08106ab34fdef318bd20e80
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-04-03 15:14:55 -0500

    updated analytics chart

--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -23,7 +23,7 @@ export const description = "An area chart with a legend"
 
 const chartData = [
   { month: "January", events: 9, people: 28 },
-  { month: "February", events: 8, people: 20 },
+  { month: "February", events: 8, people: 21 },
   { month: "March", events: 7, people: 18 },
   { month: "April", events: 0, people: 0 },
   { month: "May", events: 0, people: 0 },
