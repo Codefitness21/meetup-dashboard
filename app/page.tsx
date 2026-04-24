ac13e1641afbab2515d77738d16db90bda7e247f
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-04-23 22:30:11 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Folder, Newspaper, Users } from "lucide-react";
 import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import { getGoogleSheet } from "@/src/action";
 
+
 export default async function Home() {
   const data = await getGoogleSheet();
   console.log("Home data", data);
