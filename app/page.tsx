f04bc1c50e91de5ce8331293be050879b87f8f00
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-04-23 23:48:55 -0500

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,44 @@ export const dynamic = "force-dynamic";
 
 import DashboardCard from "@/components/dashboard/DashboardCard";
 import { Folder, Newspaper, Users } from "lucide-react";
-// import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import { getGoogleSheet } from "@/src/action";
 
 
+
 export default async function Home() {
-  // const data = await getGoogleSheet();
-  // console.log("Home data", data);
+  const data = await getGoogleSheet();
+  console.log("Home data", data);
 
-  // let currentMonth = "";
-  // const events = data?.slice(1).map((col: any) => {
-  //   if (col[0]) {
-  //     currentMonth = col[0]
-  //   }
-  //     return {
-  //       month: currentMonth,
-  //       date: col[1],
-  //       place: col[2],
-  //       id:  col[1] + col[2],
-  //     };
-  //   });
+  let currentMonth = "";
+  const events = data?.slice(1).map((col: any) => {
+    if (col[0]) {
+      currentMonth = col[0]
+    }
+      return {
+        month: currentMonth,
+        date: col[1],
+        place: col[2],
+        id:  col[1] + col[2],
+      };
+    });
 
-  // const grouped = Object.groupBy(events || [], ({ month }: any) => month);
+  const grouped = Object.groupBy(events || [], ({ month }: any) => month);
 
-  // const monthlyEvents = Object.entries(grouped).reverse().map(([month, event]: any) => {
+  const monthlyEvents = Object.entries(grouped).reverse().map(([month, event]: any) => {
 
-  //   return (
-  //   <div key={month.id}>
-  //     <div key={month.id} className="text-lg">{month}</div>
-  //     {event.map((event:any) => (
-  //         <div className="mb-2" key={event.id}>{event.date}{event.place}</div>   
-  //     ))}
-  //     </div>
-  //   );
-  // });
+    return (
+    <div key={month.id}>
+      <div key={month.id} className="text-lg">{month}</div>
+      {event.map((event:any) => (
+          <div className="mb-2" key={event.id}>{event.date}{event.place}</div>   
+      ))}
+      </div>
+    );
+  });
 
-  // const totalConnections = data?.slice(1).reduce((sum: any, col: any) => sum + Number(col[4] || 0), 0);
-  // const totalEvents = data?.slice(1).reduce((sum: any, col: any) => sum + Number(col[3] || 0), 0);
+  const totalConnections = data?.slice(1).reduce((sum: any, col: any) => sum + Number(col[4] || 0), 0);
+  const totalEvents = data?.slice(1).reduce((sum: any, col: any) => sum + Number(col[3] || 0), 0);
 
   return (
     <>
@@ -50,7 +51,7 @@ export default async function Home() {
           <div className="flex flex-row justify-center w-full opacity-90 font-bold lg:mb-0 md:mb-5 mb-5">
             <DashboardCard
               title="2026 Meetup Events"
-              // place={monthlyEvents}
+              place={monthlyEvents}
               icon={<Newspaper className="text-slate-500" />}
             />
           </div>
@@ -60,20 +61,20 @@ export default async function Home() {
           <div className="flex flex-row justify-center w-full md:flex-row mb-5 lg:h-30 h-25">
             <DashboardCard
               title="Total invested connections"
-              // data={totalConnections}
+              data={totalConnections}
               icon={<Users className=" text-slate-500" />}
             />
           </div>
           <div className="flex flex-row justify-center w-full md:flex-row mb-5 lg:h-30 h-25">
             <DashboardCard
               title="Total meetup events"
-              // data={totalEvents}
+              data={totalEvents}
               icon={<Folder className="text-slate-500" />}
             />
           </div>
         </div>
       </div>
-      {/* <AnalyticsChart /> */}
+      <AnalyticsChart />
     </>
   );
 }
--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -1,108 +1,108 @@
-// "use client"
+"use client"
 
-// import { Area, AreaChart, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from "recharts"
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from "recharts"
 
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
-// import {
-//   ChartContainer,
-//   ChartLegend,
-//   ChartLegendContent,
-//   ChartTooltip,
-//   ChartTooltipContent,
-//   type ChartConfig,
-// } from "@/components/ui/chart"
-// // import events from "@/data/events";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart"
+// import events from "@/data/events";
 
-// export const description = "An area chart with a legend"
+export const description = "An area chart with a legend"
 
-// const chartData = [
-//   { month: "January", events: 9, people: 28 },
-//   { month: "February", events: 8, people: 19 },
-//   { month: "March", events: 6, people: 20 },
-//   { month: "April", events: 6, people: 18 }, 
-// ]
+const chartData = [
+  { month: "January", events: 9, people: 28 },
+  { month: "February", events: 8, people: 19 },
+  { month: "March", events: 6, people: 20 },
+  { month: "April", events: 6, people: 18 }, 
+]
 
 
-// const chartConfig = {
-//   events: {
-//     label: "Events",
-//     color: "var(--chart-1)",
-//   },
-//   people: {
-//     label: "People",
-//     color: "var(--chart-2)",
-//   },
-// } satisfies ChartConfig
+const chartConfig = {
+  events: {
+    label: "Events",
+    color: "var(--chart-1)",
+  },
+  people: {
+    label: "People",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig
 
-// export function AnalyticsChart() {
+export function AnalyticsChart() {
   
-//   return (
-//     <Card className="opacity-90 m-1 lg:mx-14 mb-5">
-//       <CardHeader className="text-center">
-//         <CardTitle className="text-2xl font-bold">2026 Analytics</CardTitle>
-//         <CardDescription className="text-slate-900">
-//           Views Per Month
-//         </CardDescription>
-//       </CardHeader>
-//       <CardContent >
+  return (
+    <Card className="opacity-90 m-1 lg:mx-14 mb-5">
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl font-bold">2026 Analytics</CardTitle>
+        <CardDescription className="text-slate-900">
+          Views Per Month
+        </CardDescription>
+      </CardHeader>
+      <CardContent >
         
-//         <ChartContainer className="h-96 w-full sm:basis-full min-h" config={chartConfig} >
+        <ChartContainer className="h-96 w-full sm:basis-full min-h" config={chartConfig} >
           
-//           <AreaChart
-//             accessibilityLayer
-//             data={chartData}
-//             margin={{
-//               left: 12,
-//               right: 12,
-//             }}
-//           >
-//             <CartesianGrid vertical={false} />
-//             <XAxis className="font-bold"
-//               dataKey="month"
-//               tickLine={false}
-//               axisLine={false}
-//               tickMargin={10}
-//               // tickFormatter={(value) => value.slice(0, 3)}
-//             />
-//             <YAxis className="font-bold"
-//               tickLine={false}
-//               axisLine={false}
-//               tickMargin={8}
-//               // tickCount={10}
-//             />
-//             <ChartTooltip
-//               cursor={false}
-//               content={<ChartTooltipContent indicator="line" />}
-//             />
-//             <Area
-//               dataKey="people"
-//               type="natural"
-//               fill="var(--color-people)"
-//               fillOpacity={0.4}
-//               stroke="var(--color-people)"
-//               stackId="a"
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis className="font-bold"
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={10}
+              // tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <YAxis className="font-bold"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              // tickCount={10}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Area
+              dataKey="people"
+              type="natural"
+              fill="var(--color-people)"
+              fillOpacity={0.4}
+              stroke="var(--color-people)"
+              stackId="a"
             
-//             />
-//             <Area
-//               dataKey="events"
-//               type="natural"
-//               fill="var(--color-events)"
-//               fillOpacity={0.4}
-//               stroke="var(--color-events)"
-//               stackId="b"
+            />
+            <Area
+              dataKey="events"
+              type="natural"
+              fill="var(--color-events)"
+              fillOpacity={0.4}
+              stroke="var(--color-events)"
+              stackId="b"
               
-//             />
-//             <ChartLegend content={<ChartLegendContent />} />
-//           </AreaChart>
-//         </ChartContainer>
-//       </CardContent>
-//     </Card>
-//   )
-// }
-// export default AnalyticsChart;
+            />
+            <ChartLegend content={<ChartLegendContent />} />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
+export default AnalyticsChart;
