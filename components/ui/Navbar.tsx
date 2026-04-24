e81747c3e91e75ab6581f86bbc68123f68d6a538
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-04-24 12:32:17 -0500

    removed date and time from navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import logo from "@/public/img/KeLLab.png";
 import { Avatar} from "@/components/ui/avatar";
 import avatar from "@/public/img/avatar.png";
-import DigitalClock from "@/components/clock/DigitalClock";
-import TodayDate from "../date/TodayDate";
+// import DigitalClock from "@/components/clock/DigitalClock";
+// import TodayDate from "../date/TodayDate";
 
 const Navbar = () => {
   return (
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Image src={logo} alt="KeLLab Logo" width={70} />
       </Link>
      
-      <div className="flex flex-row self-center justify-right text-1xl gap-2"> <TodayDate /><DigitalClock /></div>
+      {/* <div className="flex flex-row self-center justify-right text-1xl gap-2"> <TodayDate /><DigitalClock /></div> */}
 
       <Avatar>
         <Image src={avatar} alt="avatar" width={40} className="rounded-3xl" />
