6c18cf4a9e13bcf775d86d081f34d8b427ef42e6
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-04-25 15:48:18 -0500

    adjusted margins of chart to m-3 from m-1 to provide additional spacing.

--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -34,7 +34,7 @@ const chartConfig = {
 export function AnalyticsChart({chartData}: {chartData:any}) {
   
   return (
-    <Card className="opacity-90 m-1 lg:mx-14 mb-5">
+    <Card className="opacity-90 m-3 lg:mx-14 mb-5">
       <CardHeader className="text-center">
         <CardTitle className="text-2xl font-bold">2026 Analytics</CardTitle>
         <CardDescription className="text-slate-900">
